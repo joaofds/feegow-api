e468561fb1dbcffd7a4579e24040c9cdd4d7040d
João Ferreira <jo4o.fds@gmail.com>
2022-03-23 02:57:45 -0300

    finaliza busca de profissionais

--- app/Http/Controllers/ProfessionalController.php
+++ app/Http/Controllers/ProfessionalController.php
@@ -2,8 +2,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\FeegowApiService;
 
 class ProfessionalController extends Controller
 {
-    //
+    public function __construct(FeegowApiService $feeGow)
+    {
+        $this->feeGowApi = $feeGow;
+    }
+
+    public function list(Request $request)
+    {
+        $id = $request->query('id');
+        $endPoint = '/professional/list';
+
+        $params['id'] = $id;
+        $params['endpoint'] = $endPoint;
+
+        $specialties = $this->feeGowApi->getResource($params);
+        return response($specialties);
+    }
 }
