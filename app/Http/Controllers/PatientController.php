3ca2c883e9c50d56ee17816e6cdfdb5caed9d564
João Ferreira <jo4o.fds@gmail.com>
2022-03-25 09:45:28 -0300

    finaliza metodo de insercao de dados em tabela pivot

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -29,12 +29,32 @@ class PatientController extends Controller
         return response($sourceList);
     }
 
+    /**
+     *  Insere dados em tabela pivot
+     *
+     * @param Request $request
+     * @return Json
+     */
     public function appointment(Request $request)
     {
         $data = $request->all();
+        try {
+            $result = $this->patientPivot->create($data);
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'error' => true,
+                    'message' => $e->getMessage()
+                ],
+                503
+            );
+        }
+
         return response()->json(
             [
-                $data
+                'status' => 200,
+                'message' => 'dados salvos com sucesso',
+                'data' => $result
             ],
             200
         );
--- app/Models/PatientAppointment.php
+++ app/Models/PatientAppointment.php
@@ -5,5 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PatientAppointment extends Model
 {
+    public $timestamps = false;
     protected $table = 'patient_has_appointment';
+    protected $fillable = [
+        'specialty_id',
+        'professional_id',
+        'name',
+        'cpf',
+        'birthdate',
+        'source_id',
+    ];
 }
